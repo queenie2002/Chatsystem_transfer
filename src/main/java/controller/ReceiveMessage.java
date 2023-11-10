f08772b4fe529fc87ea664c31b12bb2b1dd0bcf8
“Queenie2002” <yqnguyen@insa-toulouse.fr>
2023-11-10 13:04:40 +0100

    updating receive message and send message it can receive and send messages now :)

--- src/main/java/controller/ReceiveMessage.java
+++ src/main/java/controller/ReceiveMessage.java
@@ -7,8 +7,6 @@ si recoit message broadcast, renvoie un message disant qu’il est connecté du 
 
  */
 
-import model.*;
-import java.nio.ByteBuffer; /*pour convert int into byte*/
 import java.io.*;
 import java.net.*;
 
@@ -16,31 +14,29 @@ import java.net.*;
 
 public class ReceiveMessage extends Thread {
 
-    public void receiveBroadcast(User me) throws IOException {
 
-        //we set the address
-        InetAddress senderAddress = InetAddress.getByName("255.255.255.255");
-        int senderPort = 2000; /*le port sur lequel il devra recevoir*/
+    /* are we making a function to receive all messages and then making a if case it's a broadcast*/
+    public void run() {
+        //this receives messages
+        try {DatagramSocket receivingSocket = new DatagramSocket(2000);
+            boolean running = true;
+            byte[] buf = new byte[256];
+            while (running) {
+                DatagramPacket inPacket  = new DatagramPacket(buf, buf.length);
+                receivingSocket.receive(inPacket);
+                String received = new String(inPacket.getData(), 0, inPacket.getLength());
+                System.out.println(Thread.currentThread().getName() + " received: " + received + "<");
 
-        //create datagram socket
-        //avec le try, le socket est relâché automatiquement à la fin, donc pas besoin du close
-        try (DatagramSocket socket = new DatagramSocket(1000)) {  /*who I am, port duquel j'envoie*/
+                if (received.equals("end")) {
+                    System.out.println("DONE");
+                    running = false;
+                }
 
-            //we enable broadcasting
-            socket.setBroadcast(true);
-
-            //create message
-            byte[] buf = ByteBuffer.allocate(4).putInt(me.getId()).array(); /*the message I send, says who I am and carries info of where I am so they can send it back  */
-            /*returns the array of 4 int*/ /*can make it bigger, made it 4 bytes long for 1 int for id*/ //le message est en byte du coup, à convertir
-
-            DatagramPacket outPacket = new DatagramPacket(buf, buf.length, senderAddress, senderPort);
-            socket.send(outPacket);
-
-            System.out.println("Broadcast message sent.");
+            }
+            receivingSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-
-
-
 
 }
--- src/main/java/controller/SendMessage.java
+++ src/main/java/controller/SendMessage.java
@@ -16,12 +16,19 @@ donc faut lancer l'autre thread à partir de send broadcast
 
 on envoie toujours au port 2000 (c'est le port serveur)
  */
+
+
+
+import controller.*;
 import model.*;
 import java.nio.ByteBuffer; /*pour convert int into byte*/
 import java.io.*;
 import java.net.*;
-public class SendMessage extends Thread {
-    public void sendBroadcast(User me) throws IOException {
+
+
+
+public class SendMessage{
+    public void sendBroadcast(int id) throws IOException {  /*****pour tester mais sinon User me***/
 
         //we set the address
         InetAddress senderAddress = InetAddress.getByName("255.255.255.255");
@@ -29,27 +36,39 @@ public class SendMessage extends Thread {
 
         //create datagram socket
         //avec le try, le socket est relâché automatiquement à la fin, donc pas besoin du close
-        try (DatagramSocket socket = new DatagramSocket(1000)) {  /*who I am, port duquel j'envoie*/
+        try (DatagramSocket sendingSocket = new DatagramSocket(1200)) {  /*who I am, port duquel j'envoie*/
 
             //we enable broadcasting
-            socket.setBroadcast(true);
+            sendingSocket.setBroadcast(true);
 
             //create message broadcast, on met broadcast au début comme ça on sait que c'est un msg broadcast
             byte[] buf = new byte[256];
-            String message = "broadcast" + me.getId() ;
-            for (int i=0; i<256; i++) { /*on écrit un message à envoyer*/
-                if (i<=message.length()) {
-                    buf[i] = (byte) message.charAt(i);
-                }
+
+
+            String message = "broadcast" + id; //me.getId() ;
+            for (int i=0; i<message.length(); i++) { /*on écrit un message à envoyer*/
+                buf[i] = (byte) message.charAt(i);
             }
 
-                    ByteBuffer.allocate(4).putInt(me.getId()).array(); /*the message I send, says who I am and carries info of where I am so they can send it back  */
-            /*returns the array of 4 int*/ /*can make it bigger, made it 4 bytes long for 1 int for id*/ //le message est en byte du coup, à convertir
+
+
+           /*the message I send, says who I am and carries info of where I am so they can send it back  */
+           //le message est en byte du coup, à convertir
 
             DatagramPacket outPacket = new DatagramPacket(buf, buf.length, senderAddress, senderPort);
-            socket.send(outPacket);
+            sendingSocket.send(outPacket);
 
             System.out.println("Broadcast message sent.");
         }
     }
+
+
+
+    public static void main(String[] args) throws IOException {
+        ReceiveMessage r1 = new ReceiveMessage ();
+        SendMessage s = new SendMessage ();
+        r1.start();
+        s.sendBroadcast(123);
+    }
+
 }
