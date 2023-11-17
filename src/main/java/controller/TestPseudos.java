887126ae39fbf72715442ddaad1c626ccf419e5c
Raph <rees@insa-toulouse.fr>
2023-11-17 12:12:37 +0100

    test pseudo

--- src/main/java/controller/TestPseudos.java
+++ src/main/java/controller/TestPseudos.java
@@ -1,5 +1,19 @@
 package controller;
-import model.*;
+//import model.*;
+import model.ContactList;
+import model.User;
+
+import java.util.*;
 public class TestPseudos {
+
     User me = new User(1,"myPseudo","myFirstName","myLastName","myBday","myPword",true);
+    User newUser = new User(2,"newPseudo","newFirstName","newLastName","myBday","myPword",true);
+
+    ContactList instance = ContactList.getInstance();
+
+    public static void main(String[] args){
+        
+
+    }
+
 }
