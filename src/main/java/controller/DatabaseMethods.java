a52a2cf2b7db330d630a3db2ae0b34c7cc15188f
Raph <rees@insa-toulouse.fr>
2023-12-19 18:10:03 +0100

    modified Database Methods and added test class (doesn't work yet)

--- src/main/java/controller/DatabaseMethods.java
+++ src/main/java/controller/DatabaseMethods.java
@@ -4,70 +4,89 @@ import java.sql.*;
 
 public class DatabaseMethods {
 
-    public static String url = "jdbc:sqlite:my_database.db";
+    private static final String url = "jdbc:sqlite:my_database.db";
+    private static Connection connection = null;
+
+    public static boolean startConnection(String url) throws SQLException {
+        try {
+            connection = DriverManager.getConnection(url);
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
 
     public static void tableUsers() throws SQLException {
-        try (Connection connection = DriverManager.getConnection(url);
-             Statement statement = connection.createStatement()) {
+        if(connection == null)
+                throw new SQLException();
+         Statement statement = connection.createStatement();
 
-            String usersTableQuery = "CREATE TABLE IF NOT EXISTS Users ("
-                    + "ipAddress TEXT PRIMARY KEY AUTOINCREMENT, "
-                    + "name TEXT NOT NULL, "
-                    + "status INTEGER NOT NULL)";
+        String usersTableQuery = "CREATE TABLE IF NOT EXISTS Users ("
+                + "ipAddress TEXT PRIMARY KEY AUTOINCREMENT, "
+                + "name TEXT NOT NULL, "
+                + "status INTEGER NOT NULL)";
 
-            statement.execute(usersTableQuery);
-            System.out.println("Table Users created");
-        }
+        statement.execute(usersTableQuery);
+        System.out.println("Table Users created");
     }
 
     public static void tableMessages(String tableName) throws SQLException {
 
-        try (Connection connection = DriverManager.getConnection(url);
-             Statement statement = connection.createStatement()) {
+        if(connection == null)
+            throw new SQLException();
+         Statement statement = connection.createStatement();
 
-            String messagesTableQuery = "CREATE TABLE IF NOT EXISTS Messages_" + tableName + " ("
-                    + "messageID INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + "content TEXT NOT NULL, "
-                    + "date TEXT NOT NULL)";
+        String messagesTableQuery = "CREATE TABLE IF NOT EXISTS Messages_" + tableName + " ("
+                + "messageID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "content TEXT NOT NULL, "
+                + "date TEXT NOT NULL)";
 
-            statement.execute(messagesTableQuery);
-            System.out.println("Table Messages_" + tableName + " created");
-        }
+        statement.execute(messagesTableQuery);
+        System.out.println("Table Messages_" + tableName + " created");
+
     }
 
     public static void addUser(String ipAddress, String name, int status) throws SQLException {
         String insertUserSQL = "INSERT INTO Users (ipAddress, name, status) VALUES (?, ?, ?)";
 
-        try (Connection connection = DriverManager.getConnection(url);
-             PreparedStatement preparedStatement = connection.prepareStatement(insertUserSQL)) {
-            preparedStatement.setString(1, ipAddress);
-            preparedStatement.setString(2, name);
-            preparedStatement.setInt(3, status);
+        if(connection == null)
+            throw new SQLException();
+         PreparedStatement preparedStatement = connection.prepareStatement(insertUserSQL);
+        preparedStatement.setString(1, ipAddress);
+        preparedStatement.setString(2, name);
+        preparedStatement.setInt(3, status);
 
-            int affectedRows = preparedStatement.executeUpdate();
-            if (affectedRows > 0) {
-                System.out.println("User added successfully");
-            } else {
-                System.out.println("User could not be added");
-            }
+        int affectedRows = preparedStatement.executeUpdate();
+        if (affectedRows > 0) {
+            System.out.println("User added successfully");
+        } else {
+            System.out.println("User could not be added");
         }
+
     }
 
     public static void addMessage(String tableName, String content, String date) throws SQLException {
 
         String insertMessageSQL = "INSERT INTO Messages_" + tableName + " (content, date) VALUES (?, ?)";
 
-        try (Connection connection = DriverManager.getConnection(url);
-             PreparedStatement preparedStatement = connection.prepareStatement(insertMessageSQL)) {
-            preparedStatement.setString(1, content);
-            preparedStatement.setString(2, date);
+        if(connection == null)
+            throw new SQLException();
+         PreparedStatement preparedStatement = connection.prepareStatement(insertMessageSQL);
+        preparedStatement.setString(1, content);
+        preparedStatement.setString(2, date);
 
-            int affectedRows = preparedStatement.executeUpdate();
-            if (affectedRows > 0) {
-                System.out.println("Message added successfully to table Messages_" + tableName);
-            } else {
-                System.out.println("Message could not be added to table Messages_" + tableName);
-            }
+        int affectedRows = preparedStatement.executeUpdate();
+        if (affectedRows > 0) {
+            System.out.println("Message added successfully to table Messages_" + tableName);
+        } else {
+            System.out.println("Message could not be added to table Messages_" + tableName);
+        }
+    }
+
+    public static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
         }
     }
 
