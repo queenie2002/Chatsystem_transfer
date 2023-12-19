a5d1f2db789d5e8cac85c279362daeb86f2fcdeb
Raph <rees@insa-toulouse.fr>
2023-12-19 16:46:02 +0100

    started DatabaseMethods class

---
+++ src/main/java/controller/DatabaseMethods.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.net.InetAddress;
+import java.sql.SQLException;
+
+public class DatabaseMethods {
+    public static void tableUsers(InetAddress address) throws SQLException {
+
+    }
+
+    public static void tableMessages(InetAddress address) throws SQLException {
+
+    }
+}
