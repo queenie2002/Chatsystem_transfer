d066effd860bf0b002b355f7b4c0b758805f1cf3
“Queenie2002” <yqnguyen@insa-toulouse.fr>
2024-01-23 16:45:00 +0100

    view: choosing what to do when we close the app

--- src/main/java/chatsystem/ui/HomeTab.java
+++ src/main/java/chatsystem/ui/HomeTab.java
@@ -1,17 +1,52 @@
 package chatsystem.ui;
 
 
+import chatsystem.MainClass;
 import chatsystem.contacts.ContactList;
+import chatsystem.observers.MyObserver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class HomeTab extends JFrame {
+
+    //LOGGER
+    private static final Logger LOGGER = LogManager.getLogger(HomeTab.class);
+
+
+    //OBSERVERS
+    ArrayList<MyObserver> observers = new ArrayList<>();
+    public synchronized void addObserver(MyObserver obs) {
+        this.observers.add(obs);
+    }
+
+
+
     public HomeTab(){
+        JFrame frame = new JFrame("User Registration");
         setTitle("HomeTab");
         setSize(500,300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
+        //We don't want to close right away
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                for (MyObserver obs : observers) {
+                    try {
+                        obs.toDisconnect(frame, MainClass.me);
+                    } catch (IOException ex) {
+                        LOGGER.error("Couldn't warn others that I'm disconnected");
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });        setLayout(new FlowLayout());
 
         JButton button_startChat = new JButton("Start Chatting");
 
--- src/main/java/chatsystem/ui/Register.java
+++ src/main/java/chatsystem/ui/Register.java
@@ -2,6 +2,8 @@ package chatsystem.ui;
 
 import chatsystem.MainClass;
 import chatsystem.observers.MyObserver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,12 +13,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 
+/** Opens a Register tab */
 public class Register {
-
-    private JTextField nickname;
-    private JPasswordField password;
-
-
 
     //OBSERVERS
     ArrayList<MyObserver> observers = new ArrayList<>();
@@ -26,49 +24,61 @@ public class Register {
 
 
 
+
+    private JTextField nickname;
+    private JPasswordField password;
+
+
+
+
+
+
     public Register() {
 
         // Create and set up the window
         JFrame frame = new JFrame("User Registration");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 600);
-        frame.setLocationRelativeTo(null);
+        frame.setLocationRelativeTo(null); //center
         frame.setLayout(new BorderLayout());
 
 
-
-        JPanel panel = new JPanel(new FlowLayout());
+        //Label Panel
+        JPanel labelPanel = new JPanel(new FlowLayout());
         JLabel registerLabel = new JLabel("Register", JLabel.CENTER);
         registerLabel.setPreferredSize(new Dimension(175, 100));
-        panel.add(registerLabel);
+        labelPanel.add(registerLabel);
 
 
 
 
-
-        JPanel panel1 = new JPanel(new GridLayout(2, 2));
+        //Register Panel
+        JPanel registerPanel = new JPanel(new GridLayout(2, 2));
 
         JButton registerButton = new JButton("Register");
         registerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                //get inputs
                 String nicknameInfo = nickname.getText();
                 char[] passwordInfo = password.getPassword();
 
-
+                //notify observer
                 for (MyObserver obs : observers) {
                     obs.registerFunction(nicknameInfo,String.valueOf(passwordInfo),frame);
                 }
             }
         });
 
+
+        //create the text fields
         nickname = new JTextField(20);
         password = new JPasswordField(20);
-        panel1.add(new JLabel("Nickname:"));
-        panel1.add(nickname);
-        panel1.add(new JLabel("Password:"));
-        panel1.add(password);
+        registerPanel.add(new JLabel("Nickname:"));
+        registerPanel.add(nickname);
+        registerPanel.add(new JLabel("Password:"));
+        registerPanel.add(password);
 
 
 
@@ -76,12 +86,13 @@ public class Register {
 
 
         //Redirection Panel
-        JPanel panel2 = new JPanel(new GridLayout(1, 3));
+        JPanel redirectionPanel = new JPanel(new GridLayout(1, 3));
 
         JButton closeButton = new JButton("Close");
         closeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //notifies observers
                 for (MyObserver obs : observers) {
                     obs.toCloseApp(frame);
                 }
@@ -94,6 +105,7 @@ public class Register {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
+                    //goes to previous tab
                     Beginning beginning = new Beginning();
                     beginning.addObserver(MainClass.controller);
                 } catch (IOException ex) {
@@ -102,17 +114,16 @@ public class Register {
                 frame.dispose();
             }
         });
-
-        panel2.add(closeButton);
-        panel2.add(previousButton);
-        panel2.add(registerButton);
-        panel2.setSize(600, 100);
-
+        redirectionPanel.add(closeButton);
+        redirectionPanel.add(previousButton);
+        redirectionPanel.add(registerButton);
+        redirectionPanel.setSize(600, 100);
 
 
-        frame.add(panel, BorderLayout.PAGE_START);
-        frame.add(panel1, BorderLayout.CENTER);
-        frame.add(panel2, BorderLayout.PAGE_END);
+        //add all panels to frame
+        frame.add(labelPanel, BorderLayout.PAGE_START);
+        frame.add(registerPanel, BorderLayout.CENTER);
+        frame.add(redirectionPanel, BorderLayout.PAGE_END);
         frame.setVisible(true);
 
     }
